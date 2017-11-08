5f6498e154b6a8ae1a969a6a93b8757bbb5626da
wilfried01 <beswil88@hotmail.ch>
2017-11-08 12:39:53 +0100

    L'implémentation des charts a été modifié. À la place d'avoir plusieurs chart, il n'y a plus qu'un seul avec tous les pourcentages de connaissances des langages de programmation.

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     // On sélectionne toutes les balises avec la classe valant progress dans notre document.
     // On aurait pu choisir progress-bar directement, mais la class progress
     // n'a pas d'importance sans progress-bar. c'est pourquoi nous la supprimons complètement
-    var chartElements = $('.progress');
+    const chartElements = Array.from(document.getElementsByClassName('progress'));
 
     // Code de couleur pour chaque langage à  représenter dans notre page
     window.chartColors = {
@@ -22,74 +22,90 @@ $(document).ready(function () {
         grey: '#A4A4A4'
     };
 
-    // On parcours les éléments du tableau et à chacun on applique la fonction qui transforme
-    // le progress-bar en chart
-    chartElements.each(function (index) {
-        buildDoughnut(chartElements[index], index);
-    });
+    /*
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+    */
+    // On récupère la liste de toutes les valeurs pour les langages
+    const listValues = chartElements.map(item => parseInt(item.firstElementChild.getAttribute('aria-valuenow'), 10));
+    /*
+    let listValues = new Array(6);
 
-    // Cette fonction prend en paramètre un balise (ou groupe de balises) et la remplace
-    // par une balise canvas afin d'en construire un chart. Notez que cette balise doit avoir
-    // pour enfant une balise div avec les propriétés d'une progress-bar définies dans
-    // bootstrap. ATTENTION, CETTE FONCTION MODIFIE LE DOM DE VOTRE DOCUMENT HTML
-    function buildDoughnut(item, index) {
-        // On transforme la balise reçue en obket jQuery
-        let temp = $(item);
-
-        // On récupère le langage de programmation de la balise courante
-        let langage = temp.attr('name');
-
-        // On récupère le niveau contenu dans l'attribut aria-valuenow'
-        let niveauLangage = temp.children('div').attr('aria-valuenow');
-
-        // On crée un noeud contenant la balise canvas
-        let canvas = $('<canvas id="' + langage + '" height="50" width="50"></canvas>');
-
-        // On remplace la balise reçu en paramètre par une balise canvas
-        temp.replaceWith(canvas);
-
-        // Tableau des couleurs pour l'affichage des langages
-        let couleur = [
-            window.chartColors.yellow,
-            window.chartColors.green,
-            window.chartColors.red,
-            window.chartColors.blue,
-            window.chartColors.purple,
-            window.chartColors.orange
-        ];
-
-        // La configuration de notre chart de type doughnut
-        let chartConfig = {
-            type: 'doughnut',
-            data: {
-                datasets: [{
-                    data: [
-                        niveauLangage,
-                        100 - niveauLangage
-                    ],
-                    backgroundColor: [
-                        couleur[index]
-                    ],
-                    borderWidth: 5
-                }],
-                labels: [
-                    langage,
-                    'Restant'
-                ]
-            },
-            options: {
-                responsive: true,
-                legend: {
-                    position: 'bottom'
-                },
-                animation: {
-                    animateScale: true,
-                    animateRotate: true
-                }
-            }
-        };
-
-        // Création et affichage de notre chart doughnut dans la page HTML.
-        window.Doughnut = new Chart(document.getElementById(langage).getContext('2d'), chartConfig);
+    for (let i = 0; i < listValues.length; ++i) {
+        listValues[i] = getRandomIntInclusive(20, 100);
     }
+    */
+
+    const TOTAL = listValues.reduce((sum, value) => sum + value, 0);
+
+    // On récupère le nom de tous les langages dans les progress bar
+    const listNames = chartElements.map(item => item.getAttribute('name'));
+
+    // On crée un canvas qui contiendra le doughnut
+    const canvas = $('<canvas id="' + 'progression' + '"></canvas>');
+
+    // On la remplace par la balise canvas.
+    $('#langages').replaceWith(canvas);
+
+    const couleur = [
+        window.chartColors.yellow,
+        window.chartColors.green,
+        window.chartColors.red,
+        window.chartColors.blue,
+        window.chartColors.purple,
+        window.chartColors.orange
+    ];
+
+    const data = {
+        datasets: [{
+            data: [
+                listValues[0] * 100 / TOTAL,
+                listValues[1] * 100 / TOTAL,
+                listValues[2] * 100 / TOTAL,
+                listValues[3] * 100 / TOTAL,
+                listValues[4] * 100 / TOTAL,
+                listValues[5] * 100 / TOTAL
+            ],
+            backgroundColor: [
+                couleur[0],
+                couleur[1],
+                couleur[2],
+                couleur[3],
+                couleur[4],
+                couleur[5]
+            ],
+            borderWidth: 3
+        }],
+        labels: [
+            listNames[0],
+            listNames[1],
+            listNames[2],
+            listNames[3],
+            listNames[4],
+            listNames[5]
+        ]
+    };
+
+    const options = {
+        responsive: true,
+        legend: {
+            position: 'bottom'
+        },
+        animation: {
+            animateScale: true,
+            animateRotate: true
+        },
+        cutoutPercentage: 65
+    };
+
+    const chartConfig = {
+        type: 'doughnut',
+        data,
+        options
+    };
+
+    window.Doughnut = new Chart(document.getElementById('progression').getContext('2d'), chartConfig);
 });
